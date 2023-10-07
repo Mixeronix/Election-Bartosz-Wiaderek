d480d6d9e2579d2cbd5302226b91457d979688eb
Bartosz Wiaderek <bartoszwiaderek@gmail.com>
2023-10-07 23:13:53 +0200

    Google search

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +12,11 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="en">
-			<body className={inter.className}>{children}</body>
+<Head>
+<meta name="google-site-verification" content="mkpkk0-v-7pa07PdOkNYpp4mJJfTaaNzN8TV3hzt6fI" />
+			</Head>
+
+<body className={inter.className}>{children}</body>
 		</html>
 	);
 }
