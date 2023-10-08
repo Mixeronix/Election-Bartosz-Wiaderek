b869541fbbb72dee2c328da1d3e65960234cf3d5
Bartosz Wiaderek <bartoszwiaderek@gmail.com>
2023-10-08 11:59:45 +0200

    Google verification fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,21 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
-import Head from 'next/head'
+import type { Metadata } from "next";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
+export const metadata: Metadata = {
 	title: "Bartosz Wiaderek",
 	description: "Bartosz Wiaderek portfolio",
+	verification: {
+		google: "mkpkk0-v-7pa07PdOkNYpp4mJJfTaaNzN8TV3hzt6fI",
+	},
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="en">
-<Head>
-<meta name="google-site-verification" content="mkpkk0-v-7pa07PdOkNYpp4mJJfTaaNzN8TV3hzt6fI" />
-			</Head>
-
-<body className={inter.className}>{children}</body>
+			<body className={inter.className}>{children}</body>
 		</html>
 	);
 }
